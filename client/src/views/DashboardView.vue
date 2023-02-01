43de8505a2f86b5f5a1c8c15684d9c9a962f2db1
Chris Lovell <clovellbsc@gmail.com>
2023-02-01 16:15:04 +0000

    basic dashboard  implemented with cardList

---
+++ client/src/views/DashboardView.vue
@@ -0,0 +1,25 @@
+<template>
+  <main class="home-container">
+    <CardList />
+  </main>
+</template>
+
+<script>
+import CardList from "@/components/CardList.vue";
+
+export default {
+  name: "DashboardView",
+  components: {
+    CardList,
+  },
+};
+</script>
+
+<style>
+main > .home-container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+</style>
